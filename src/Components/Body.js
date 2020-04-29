5dd1d6227d70ab38368eb5085a364e9cf644e6e0
sofiamurray <sofiamurray@umail.ucsb.edu>
2020-04-28 23:10:45 -0700

    hw 4 - update

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -9,15 +9,19 @@ class Body extends Component {
     displayContent = () => {
         var activeTab = this.props.activeTab
         if(activeTab == 1){
+            document.title="Home | Sofia's Corner"
             return <Home/>
         }
         else if(activeTab == 2){
+            document.title="Travel | Sofia's Corner"
             return <Images/>
         }
         else if(activeTab == 3){
+            document.title="Music | Sofia's Corner"
             return <Videos/>
         }
         else if(activeTab == 4){
+            document.title="Cooking| Sofia's Corner"
             return <ExtLinks/>
         }
     }
