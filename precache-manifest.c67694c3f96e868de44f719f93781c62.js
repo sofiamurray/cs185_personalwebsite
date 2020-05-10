62b6f5c5013dc07b7d78e9803219ad3f40cd71c6
sofiamurray <sofiamurray@umail.ucsb.edu>
2020-05-09 21:06:29 -0700

    Updates

---
+++ precache-manifest.c67694c3f96e868de44f719f93781c62.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1119d47541877bef2da87580dd582995",
+    "url": "/cs185_personalwebsite/index.html"
+  },
+  {
+    "revision": "c823454790ee4f30997c",
+    "url": "/cs185_personalwebsite/static/css/main.784232f7.chunk.css"
+  },
+  {
+    "revision": "3bba45fc0f35b21780fb",
+    "url": "/cs185_personalwebsite/static/js/2.e76e3ce5.chunk.js"
+  },
+  {
+    "revision": "a17b95a8c249d5592103887f8cc9d740",
+    "url": "/cs185_personalwebsite/static/js/2.e76e3ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c823454790ee4f30997c",
+    "url": "/cs185_personalwebsite/static/js/main.0c9f299f.chunk.js"
+  },
+  {
+    "revision": "469b6275ad26af743f05",
+    "url": "/cs185_personalwebsite/static/js/runtime-main.e5db6549.js"
+  },
+  {
+    "revision": "176eb723cc762f693c6ee44e0ecfd476",
+    "url": "/cs185_personalwebsite/static/media/barca_fountain.176eb723.jpg"
+  },
+  {
+    "revision": "9721cfbd73f46d5dc9c20dd361d44299",
+    "url": "/cs185_personalwebsite/static/media/barca_street.9721cfbd.jpg"
+  },
+  {
+    "revision": "f1a99ced8f1709ffe35641562844315e",
+    "url": "/cs185_personalwebsite/static/media/buda_emma.f1a99ced.jpg"
+  },
+  {
+    "revision": "780304964b4a1d5f79dcbf7015442121",
+    "url": "/cs185_personalwebsite/static/media/buda_view.78030496.jpg"
+  },
+  {
+    "revision": "fd347ec0def7d495db2f311c18297258",
+    "url": "/cs185_personalwebsite/static/media/coleslaw.fd347ec0.jpg"
+  },
+  {
+    "revision": "244982221b11c20e37eadd6973086477",
+    "url": "/cs185_personalwebsite/static/media/dublin_bar.24498222.jpg"
+  },
+  {
+    "revision": "8c35a53e995af6fc1b8b74cf1d2fa5d6",
+    "url": "/cs185_personalwebsite/static/media/dublin_cliffs.8c35a53e.jpg"
+  },
+  {
+    "revision": "efa2362998b3ab4097eae347f3447fe1",
+    "url": "/cs185_personalwebsite/static/media/madrid_leaf.efa23629.jpg"
+  },
+  {
+    "revision": "2bd529897662fde9d58bf427177e5f15",
+    "url": "/cs185_personalwebsite/static/media/madrid_street.2bd52989.jpg"
+  },
+  {
+    "revision": "e741e77d179e53a4cf5be68ea6cfb148",
+    "url": "/cs185_personalwebsite/static/media/munich_building.e741e77d.jpg"
+  },
+  {
+    "revision": "c847d08034da87d46b55e67943e4c427",
+    "url": "/cs185_personalwebsite/static/media/shrimp_foil.c847d080.jpg"
+  },
+  {
+    "revision": "2316142c3475d2802c335ceb1c7711df",
+    "url": "/cs185_personalwebsite/static/media/shrimp_scampi.2316142c.jpg"
+  },
+  {
+    "revision": "a663d6436a23c99d5f3b7f07517b5ec3",
+    "url": "/cs185_personalwebsite/static/media/sofia_sf.a663d643.jpg"
+  },
+  {
+    "revision": "b9dd7f5bc85b63633512724bf90d6c47",
+    "url": "/cs185_personalwebsite/static/media/valencia_paella.b9dd7f5b.jpg"
+  },
+  {
+    "revision": "4a5f826d6c93f7daea2bbdb720942c32",
+    "url": "/cs185_personalwebsite/static/media/valencia_street.4a5f826d.jpg"
+  },
+  {
+    "revision": "dfb435bf42e723e951dc272d97d76650",
+    "url": "/cs185_personalwebsite/static/media/zurich_cat.dfb435bf.jpg"
+  },
+  {
+    "revision": "4bac2725dc9be59fca3c17793c0f5b00",
+    "url": "/cs185_personalwebsite/static/media/zurich_house.4bac2725.jpg"
+  }
+]);
